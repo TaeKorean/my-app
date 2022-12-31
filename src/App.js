9552b653f658255be4b315f9bd7e4ce35da3eb4a
Tae <hongsangtae@gmail.com>
2022-12-31 15:03:06 +0900

    Intro to Props

--- src/App.js
+++ src/App.js
@@ -1,43 +1,48 @@
 import './App.css'
-import { useState } from 'react'
+import {useState} from 'react'
 import Title from './components/Title'
 
 function App() {
-  const [showEvents, setShowEvents] = useState(true)
-  const [events, setEvents] = useState([
-    {title: "mario's birthday bash", id: 1},
-    {title: "bowser's live stream", id: 2},
-    {title: "race on moo moo farm", id: 3}
-  ])
+	const [showEvents, setShowEvents] = useState(true)
+	const [events, setEvents] = useState([
+		{title: "mario's birthday bash", id: 1},
+		{title: "bowser's live stream", id: 2},
+		{title: 'race on moo moo farm', id: 3},
+	])
 
-  const handleClick = (id) => {
-    setEvents(prevEvents => {
-      return prevEvents.filter(event => id !== event.id)
-    })
-  }
+	const handleClick = id => {
+		setEvents(prevEvents => {
+			return prevEvents.filter(event => id !== event.id)
+		})
+	}
 
-  return (
-    <div className="App">
-      <Title />
-      
-      {showEvents && (
-        <div>
-          <button onClick={() => setShowEvents(false)}>Hide Events</button>
-        </div>
-      )}
-      {!showEvents && (
-        <div>
-          <button onClick={() => setShowEvents(true)}>Show Events</button>
-        </div>
-      )}
-      {showEvents && events.map((event, index) => (
-        <div key={event.id}>
-          <h2>{index} - {event.title}</h2>
-          <button onClick={() => handleClick(event.id)}>delete event</button>
-        </div>
-      ))}
-    </div>
-  );
+	const subtitle = 'All the lastest events in Marioland'
+
+	return (
+		<div className="App">
+			<Title title="Events in Your Area" subtitle={subtitle} />
+
+			{showEvents && (
+				<div>
+					<button onClick={() => setShowEvents(false)}>Hide Events</button>
+				</div>
+			)}
+			{!showEvents && (
+				<div>
+					<button onClick={() => setShowEvents(true)}>Show Events</button>
+				</div>
+			)}
+			{showEvents &&
+				events.map((event, index) => (
+					<div key={event.id}>
+						<h2>
+							{index} - {event.title}
+						</h2>
+						<button onClick={() => handleClick(event.id)}>delete event</button>
+					</div>
+				))}
+		</div>
+	)
 }
 
-export default App;
+export default App
--- src/components/Title.js
+++ src/components/Title.js
@@ -1,9 +1,9 @@
-export default function Title() {
-  return (
-    <div>
-      <h1 className="title">Mario Kingdom Events</h1>
-      <br />
-      <h2 className="subtitle">All the latest events in mario kingdom</h2>
-    </div>
-  )
+export default function Title({title, subtitle}) {
+	return (
+		<div>
+			<h1 className="title">{title}</h1>
+			<br />
+			<h2 className="subtitle">{subtitle}</h2>
+		</div>
+	)
 }
