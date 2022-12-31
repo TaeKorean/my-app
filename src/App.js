f924a4d84f035b59ae77c613cb0bc4b29842338c
Tae <hongsangtae@gmail.com>
2022-12-31 15:34:55 +0900

    Children props

--- src/App.js
+++ src/App.js
@@ -1,48 +1,56 @@
 import './App.css'
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import Title from './components/Title'
+import Modal from './components/Modal'
 
 function App() {
-	const [showEvents, setShowEvents] = useState(true)
-	const [events, setEvents] = useState([
-		{title: "mario's birthday bash", id: 1},
-		{title: "bowser's live stream", id: 2},
-		{title: 'race on moo moo farm', id: 3},
-	])
+  const [showEvents, setShowEvents] = useState(true)
+  const [events, setEvents] = useState([
+    {title: "mario's birthday bash", id: 1},
+    {title: "bowser's live stream", id: 2},
+    {title: "race on moo moo farm", id: 3}
+  ])
 
-	const handleClick = id => {
-		setEvents(prevEvents => {
-			return prevEvents.filter(event => id !== event.id)
-		})
-	}
+  const handleClick = (id) => {
+    setEvents(prevEvents => {
+      return prevEvents.filter(event => id !== event.id)
+    })
+  }
 
-	const subtitle = 'All the lastest events in Marioland'
+  const subtitle = "All the latest events in Marioland"
 
-	return (
-		<div className="App">
-			<Title title="Events in Your Area" subtitle={subtitle} />
+  return (
+    <div className="App">
+      <Title title="Marioland Events" subtitle={subtitle} />
+      
+      {showEvents && (
+        <div>
+          <button onClick={() => setShowEvents(false)}>Hide Events</button>
+        </div>
+      )}
+      {!showEvents && (
+        <div>
+          <button onClick={() => setShowEvents(true)}>Show Events</button>
+        </div>
+      )}
+      {showEvents && events.map((event, index) => (
+        <React.Fragment key={event.id}>
+          <h2>{index} - {event.title}</h2>
+          <button onClick={() => handleClick(event.id)}>delete event</button>
+        </React.Fragment>
+      ))}
 
-			{showEvents && (
-				<div>
-					<button onClick={() => setShowEvents(false)}>Hide Events</button>
-				</div>
-			)}
-			{!showEvents && (
-				<div>
-					<button onClick={() => setShowEvents(true)}>Show Events</button>
-				</div>
-			)}
-			{showEvents &&
-				events.map((event, index) => (
-					<React.Fragment key={event.id}>
-						<h2>
-							{index} - {event.title}
-						</h2>
-						<button onClick={() => handleClick(event.id)}>delete event</button>
-					</React.Fragment>
-				))}
-		</div>
-	)
+      {/* <Modal>
+        <h2>10% Off Coupon Code!!</h2>
+        <p>Use the code NINJA10 at the checkout.</p>
+      </Modal> */}
+      <Modal>
+        <h2>Terms and Conditions</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error odit nam et reprehenderit quibusdam temporibus officia dolorum quo sint nemo quis, laborum, quasi nisi fugit praesentium debitis repudiandae! Sapiente, omnis.</p>
+        <a href="#">find out more...</a>
+      </Modal>
+    </div>
+  );
 }
 
-export default App
+export default App;
