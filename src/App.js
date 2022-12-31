fac41c78d8b6290b0e838a8d88f22ab1f4b0cd62
Tae <hongsangtae@gmail.com>
2022-12-31 09:52:37 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import './App.css'
+import {useState} from 'react'
+
+function App() {
+	const [events, setEvents] = useState([
+		{title: "mario's birthday bash", id: 1},
+		{title: "bowser's live stream", id: 2},
+		{title: 'race on moo moo farm', id: 3},
+	])
+
+	const handleClick = id => {
+		setEvents(prevEvents => {
+			return prevEvents.filter(event => event.id !== id)
+		})
+	}
+
+	return (
+		<div className="App">
+			{events.map((event, index) => (
+				<div key={event.id}>
+					<h2>
+						{index} - {event.title}
+						<button onClick={() => handleClick(event.id)}>delete event</button>
+					</h2>
+				</div>
+			))}
+		</div>
+	)
+}
+
+export default App
