9b8eeeef8896e1ec1ddd894c8937372af23f8767
Tae <hongsangtae@gmail.com>
2023-01-01 11:02:25 +0900

    Reusable Event List Component

--- src/App.js
+++ src/App.js
@@ -1,62 +1,63 @@
 import './App.css'
-import React, { useState } from 'react'
+import {useState} from 'react'
 import Title from './components/Title'
 import Modal from './components/Modal'
+import EventList from './components/EventList'
 
 function App() {
-  const [showModal, setShowModal] = useState(true)
-  const [showEvents, setShowEvents] = useState(true)
-  const [events, setEvents] = useState([
-    {title: "mario's birthday bash", id: 1},
-    {title: "bowser's live stream", id: 2},
-    {title: "race on moo moo farm", id: 3}
-  ])
+	const [showModal, setShowModal] = useState(true)
+	const [showEvents, setShowEvents] = useState(true)
+	const [events, setEvents] = useState([
+		{title: "mario's birthday bash", id: 1},
+		{title: "bowser's live stream", id: 2},
+		{title: 'race on moo moo farm', id: 3},
+	])
 
-  const handleClick = (id) => {
-    setEvents(prevEvents => {
-      return prevEvents.filter(event => id !== event.id)
-    })
-  }
+	const handleClick = id => {
+		setEvents(prevEvents => {
+			return prevEvents.filter(event => id !== event.id)
+		})
+	}
 
-  const handleClose = () => {
-    setShowModal(false)
-  }
+	const handleClose = () => {
+		setShowModal(false)
+	}
 
-  const subtitle = "All the latest events in Marioland"
+	const subtitle = 'All the latest events in Marioland'
 
-  return (
-    <div className="App">
-      <Title title="Marioland Events" subtitle={subtitle} />
-      
-      {showEvents && (
-        <div>
-          <button onClick={() => setShowEvents(false)}>Hide Events</button>
-        </div>
-      )}
-      {!showEvents && (
-        <div>
-          <button onClick={() => setShowEvents(true)}>Show Events</button>
-        </div>
-      )}
-      {showEvents && events.map((event, index) => (
-        <React.Fragment key={event.id}>
-          <h2>{index} - {event.title}</h2>
-          <button onClick={() => handleClick(event.id)}>delete event</button>
-        </React.Fragment>
-      ))}
+	return (
+		<div className="App">
+			<Title title="Marioland Events" subtitle={subtitle} />
 
-      {showModal && (
-        <Modal handleClose={handleClose}>
-          <h2>Terms and Conditions</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error odit nam et reprehenderit quibusdam temporibus officia dolorum quo sint nemo quis, laborum, quasi nisi fugit praesentium debitis repudiandae! Sapiente, omnis.</p>
-        </Modal>
-      )}
+			{showEvents && (
+				<div>
+					<button onClick={() => setShowEvents(false)}>Hide Events</button>
+				</div>
+			)}
+			{!showEvents && (
+				<div>
+					<button onClick={() => setShowEvents(true)}>Show Events</button>
+				</div>
+			)}
+			{showEvents && <EventList events={events} handleClick={handleClick} />}
 
-      <div>
-        <button onClick={() => setShowModal(true)}>Show Modal</button>
-      </div>
-    </div>
-  );
+			{showModal && (
+				<Modal handleClose={handleClose}>
+					<h2>Terms and Conditions</h2>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error odit
+						nam et reprehenderit quibusdam temporibus officia dolorum quo sint
+						nemo quis, laborum, quasi nisi fugit praesentium debitis
+						repudiandae! Sapiente, omnis.
+					</p>
+				</Modal>
+			)}
+
+			<div>
+				<button onClick={() => setShowModal(true)}>Show Modal</button>
+			</div>
+		</div>
+	)
 }
 
-export default App;
+export default App
