420672732cccc29480c960a52afe2bc88e4eef87
Tae <hongsangtae@gmail.com>
2023-01-01 11:52:20 +0900

    Using Global Stylesheets

--- src/components/EventList.js
+++ src/components/EventList.js
@@ -1,13 +1,14 @@
 import React from 'react'
-const EventList = ({events, handleClick}) => {
-	return events.map((event, index) => (
-		<React.Fragment key={event.id}>
-			<h2>
-				{index} - {event.title}
-			</h2>
-			<button onClick={() => handleClick(event.id)}>delete event</button>
-		</React.Fragment>
-	))
-}
 
-export default EventList
+export default function EventList({ events, handleClick }) {
+  return (
+    <div>
+      {events.map((event, index) => (
+        <React.Fragment key={event.id}>
+          <h2>{index} - {event.title}</h2>
+          <button onClick={() => handleClick(event.id)}>delete event</button>
+        </React.Fragment>
+      ))}
+    </div>
+  )
+}
