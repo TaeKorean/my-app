4e703a5ed8a495e601cbee30b9728236c1cbb20d
Tae <hongsangtae@gmail.com>
2023-01-08 12:03:49 +0900

    Adding a matched Property to Cards

--- src/App.js
+++ src/App.js
@@ -1,57 +1,86 @@
+import { useState, useEffect } from 'react'
 import './App.css'
-import React, { useState } from 'react'
-import Title from './components/Title'
-import Modal from './components/Modal'
-import EventList from './components/EventList'
-import NewEventForm from './components/NewEventForm'
+import SingleCard from './components/SingleCard'
+
+const cardImages = [
+  { "src": "/img/helmet-1.png", matched: false },
+  { "src": "/img/potion-1.png", matched: false },
+  { "src": "/img/ring-1.png", matched: false },
+  { "src": "/img/scroll-1.png", matched: false },
+  { "src": "/img/shield-1.png", matched: false },
+  { "src": "/img/sword-1.png", matched: false },
+]
 
 function App() {
-  const [showModal, setShowModal] = useState(false)
-  const [showEvents, setShowEvents] = useState(true)
-  const [events, setEvents] = useState([])
+  const [cards, setCards] = useState([])
+  const [turns, setTurns] = useState(0)
+  const [choiceOne, setChoiceOne] = useState(null)
+  const [choiceTwo, setChoiceTwo] = useState(null)
 
-  const addEvent = (event) => {
-    setEvents(prevEvents => {
-      return [...prevEvents, event]  
-    })
-    setShowModal(false)
+  // shuffle cards for new game
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map(card => ({ ...card, id: Math.random() }))
+      
+    setCards(shuffledCards)
+    setTurns(0)
   }
 
-  const handleClick = (id) => {
-    setEvents(prevEvents => {
-      return prevEvents.filter(event => id !== event.id)
-    })
+  // handle a choice
+  const handleChoice = (card) => {
+    console.log(card)
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
   }
 
-  const subtitle = "All the latest events in Marioland"
+  // compare 2 selected cards
+  useEffect(() => {
+    if (choiceOne && choiceTwo) {
+
+      if (choiceOne.src === choiceTwo.src) {
+        setCards(prevCards => {
+          return prevCards.map(card => {
+            if (card.src === choiceOne.src) {
+              return { ...card, matched: true }
+            } else {
+              return card
+            }
+          })
+        })
+        resetTurn()
+      } else {
+        resetTurn()
+      }
+
+    }
+  }, [choiceOne, choiceTwo])
+
+  console.log(cards)
+
+  // reset choices & increase turn
+  const resetTurn = () => {
+    setChoiceOne(null)
+    setChoiceTwo(null)
+    setTurns(prevTurns => prevTurns + 1)
+  }
 
   return (
     <div className="App">
-      <Title title="Marioland Events" subtitle={subtitle} />
-      
-      {showEvents && (
-        <div>
-          <button onClick={() => setShowEvents(false)}>Hide Events</button>
-        </div>
-      )}
-      {!showEvents && (
-        <div>
-          <button onClick={() => setShowEvents(true)}>Show Events</button>
-        </div>
-      )}
-      {showEvents && <EventList events={events} handleClick={handleClick} />}
-      
-      {showModal && (
-        <Modal>
-          <NewEventForm addEvent={addEvent} />
-        </Modal>
-      )}
+      <h1>Magic Match</h1>
+      <button onClick={shuffleCards}>New Game</button>
 
-      <div>
-        <button onClick={() => setShowModal(true)}>Add New Event</button>
+      <div className="card-grid">
+        {cards.map(card => (
+          <SingleCard 
+            key={card.id}
+            card={card}
+            handleChoice={handleChoice}
+          />
+        ))}
       </div>
+
     </div>
   );
 }
 
-export default App;
+export default App
